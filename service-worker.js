1946818d9a33dc728e67b2b6f3803d377ad8fd8f
masterphum07-web <masterphum07-web@users.noreply.github.com>
2026-09-09 21:33:53 +0700

    Import positioning data and compact guide dialog

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE='radpose-v6';const ASSETS=['./','./index.html','./css/style.css','./js/app.js','./data/positions.json','./manifest.json','./assets/images/chest-pa-positioning.jpg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(n=>{const copy=n.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return n}).catch(()=>caches.match('./index.html')))));
+const CACHE='radpose-v7';const ASSETS=['./','./index.html','./css/style.css','./js/app.js','./data/positions.json','./data/additional-positions.json','./manifest.json','./assets/images/chest-pa-positioning.jpg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(n=>{const copy=n.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return n}).catch(()=>caches.match('./index.html')))));
+
 
 
 
